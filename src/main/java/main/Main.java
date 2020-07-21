4039ccac5adec4771c5b36a08cc38417472d6195
Jose Manuel Martinez <2015378@ce.pucmm.edu.do>
2020-07-21 18:26:19 -0400

    probandoColecciones

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,12 +2,17 @@ package main;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class Main {
     public static void main(String[] args){
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-        MongoDatabase database = mongoClient.getDatabase("suplidora");
+        MongoDatabase database = mongoClient.getDatabase("compras");
+        MongoCollection<Document> collection = database.getCollection("Ordenes");
+        long countDocuments = collection.countDocuments();
+        System.out.println(countDocuments);
     }
 
 }
